0b51cfa269bbab68991022deb5426d26802abe5a
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-06 15:57:15 -0500

    template for models

---
+++ munge/04-models.R
@@ -0,0 +1,24 @@
+###############################################################################
+# TO BE EDITED
+#
+# Authors:
+# Paul Hendricks
+#
+# Date:
+# 11-06-2015
+#
+# inputs:
+# foo.csv
+#
+# outputs:
+# bar.RData
+###############################################################################
+
+# Load libraries
+library(forecast)
+
+# Load data
+train <- 
+  read.csv("../data/prepped/train.csv", 
+           stringsAsFactors = FALSE)
+
